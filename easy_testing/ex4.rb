7eab86838648ba6cf08d87f724d8274e9b94f110
Benjamin Perrault <benjamin.perrault@gmail.com>
2021-03-18 09:23:35 -0500

    Add ex4 of easy testing

---
+++ easy_testing/ex4.rb
@@ -0,0 +1,18 @@
+# Write a minitest assertion that will fail if the Array list is not empty.
+
+require 'minitest/autorun'
+require 'minitest/reporters'
+MiniTest::Reporters.use!
+
+require_relative 'file_to_test'
+
+class ArrayTest << MiniTest::Test
+  def setup
+    list = [1, 2, 3]
+  end
+
+  def test_array_not_empty
+    assert_equal(true, list.empty?)
+    assert_empty(list)
+  end
+end

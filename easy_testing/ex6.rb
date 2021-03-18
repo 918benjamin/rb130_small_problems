9004894ac1f738580cb5ba99483e4c1e67b43e52
Benjamin Perrault <benjamin.perrault@gmail.com>
2021-03-18 09:27:35 -0500

    Add ex6 of easy testing

---
+++ easy_testing/ex6.rb
@@ -0,0 +1,18 @@
+# Write a minitest assertion that will fail unless employee.hire raises a
+# NoExperienceError exception.
+
+require 'minitest/autorun'
+require 'minitest/reporters'
+MiniTest::Reporters.use
+
+require_relative 'file_to_test'
+
+class ExceptionTest < MiniTest::Test
+  def setup
+    employee = Employee.new
+  end
+
+  def test_no_experience_error
+    assert_raises(NoExperienceError) { employee.hire }
+  end
+end

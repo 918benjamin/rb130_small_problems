d66f5f7514929fa6b03eb7a46c1e9d769b527922
Benjamin Perrault <benjamin.perrault@gmail.com>
2021-03-18 09:30:18 -0500

    Add ex7 of easy testing

---
+++ easy_testing/ex7.rb
@@ -0,0 +1,19 @@
+# Write a minitest assertion that will fail if value is not an instance of the
+# Numeric class exactly. value may not be an instance of one of Numeric's superclasses.
+
+require 'minitest/autorun'
+require 'minitest/reporters'
+MiniTest::Reporters.use!
+
+require_relative 'file_to_test'
+
+class TypeAssertions < MiniTest::Test
+  def setup
+    value = 1
+  end
+
+  def test_numeric
+    assert_equal(Numeric, value.class)
+    assert_instance_of(Numeric, value)
+  end
+end

41840432b38e466ec561cf4b5358ca737687e30b
Benjamin Perrault <benjamin.perrault@gmail.com>
2021-03-23 12:17:34 -0500

    Refactor Transaction#prompt_for_payment test to supress stdout

--- medium2_testing/test_transaction.rb
+++ medium2_testing/test_transaction.rb
@@ -7,13 +7,12 @@ require_relative 'transaction'
 class TransactionTest < MiniTest::Test
   def setup
     @transaction = Transaction.new(50)
-    @input1 = StringIO.new("30\n")
-    @input2 = StringIO.new("75\n")
-    @input3 = StringIO.new("Hi\n")
+    @input = StringIO.new("75\n")
+    @output = StringIO.new
   end
 
   def test_prompt_for_payment
-    @transaction.prompt_for_payment(input: @input2)
+    @transaction.prompt_for_payment(input: @input, output: @output)
     assert_equal(75, @transaction.amount_paid)
   end
 end

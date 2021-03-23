2fc48b1f485e950179f92acd645eb9603f213af1
Benjamin Perrault <benjamin.perrault@gmail.com>
2021-03-23 11:25:43 -0500

    Add further exploration for ex7 of medium1

--- medium1/ex7.rb
+++ medium1/ex7.rb
@@ -4,16 +4,62 @@ block that determines which of two consecutive elements will appear first in the
 
 =end
 
+# def bubble_sort!(array)
+#   loop do
+#     swapped = false
+#     1.upto(array.size - 1) do |index|
+#       conditional_check = if block_given?
+#                             yield(array[index - 1], array[index])
+#                           else
+#                             array[index - 1] <= array[index]
+#                           end
+#       next if conditional_check
+#       array[index - 1], array[index] = array[index], array[index - 1]
+#       swapped = true
+#     end
+
+#     break unless swapped
+#   end
+#   nil
+# end
+
+# array = [5, 3]
+# bubble_sort!(array)
+# p array == [3, 5]
+
+# array = [5, 3, 7]
+# bubble_sort!(array) { |first, second| first >= second }
+# p array == [7, 5, 3]
+
+# array = [6, 2, 7, 1, 4]
+# bubble_sort!(array)
+# p array == [1, 2, 4, 6, 7]
+
+# array = [6, 12, 27, 22, 14]
+# bubble_sort!(array) { |first, second| (first % 7) <= (second % 7) }
+# p array == [14, 22, 12, 6, 27]
+
+# array = %w(sue Pete alice Tyler rachel Kim bonnie)
+# bubble_sort!(array)
+# p array == %w(Kim Pete Tyler alice bonnie rachel sue)
+
+# array = %w(sue Pete alice Tyler rachel Kim bonnie)
+# bubble_sort!(array) { |first, second| first.downcase <= second.downcase }
+# p array == %w(alice bonnie Kim Pete rachel sue Tyler)
+
+# Further Exploration
+# Modify your solution so it only passes one element to the block at a time; the
+# block should perform some sort of transformation to the argument, and #bubble_sort!
+# itself should just use <= to compare two values. In other words, you should be
+# able to call #bubble_sort! like this:
+
+
+
 def bubble_sort!(array)
   loop do
     swapped = false
     1.upto(array.size - 1) do |index|
-      conditional_check = if block_given?
-                            yield(array[index - 1], array[index])
-                          else
-                            array[index - 1] <= array[index]
-                          end
-      next if conditional_check
+      next if yield(array[index - 1]) <= yield(array[index])
       array[index - 1], array[index] = array[index], array[index - 1]
       swapped = true
     end
@@ -23,26 +69,6 @@ def bubble_sort!(array)
   nil
 end
 
-array = [5, 3]
-bubble_sort!(array)
-p array == [3, 5]
-
-array = [5, 3, 7]
-bubble_sort!(array) { |first, second| first >= second }
-p array == [7, 5, 3]
-
-array = [6, 2, 7, 1, 4]
-bubble_sort!(array)
-p array == [1, 2, 4, 6, 7]
-
-array = [6, 12, 27, 22, 14]
-bubble_sort!(array) { |first, second| (first % 7) <= (second % 7) }
-p array == [14, 22, 12, 6, 27]
-
 array = %w(sue Pete alice Tyler rachel Kim bonnie)
-bubble_sort!(array)
-p array == %w(Kim Pete Tyler alice bonnie rachel sue)
-
-array = %w(sue Pete alice Tyler rachel Kim bonnie)
-bubble_sort!(array) { |first, second| first.downcase <= second.downcase }
+bubble_sort!(array) { |value| value.downcase }
 p array == %w(alice bonnie Kim Pete rachel sue Tyler)

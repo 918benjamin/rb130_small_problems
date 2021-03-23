225a1dec3543d03c8ca912620b8ff65a5296fb64
Benjamin Perrault <benjamin.perrault@gmail.com>
2021-03-23 11:40:11 -0500

    Add test_accept_money test

--- medium2_testing/test_cash_register.rb
+++ medium2_testing/test_cash_register.rb
@@ -6,5 +6,15 @@ require_relative 'cash_register'
 require_relative 'transaction'
 
 class CashRegisterTest < MiniTest::Test
+  def setup
+    @register = CashRegister.new(500)
+    @transaction = Transaction.new(50)
+  end
 
+  def test_accept_money
+    assert_equal(500, @register.total_money)
+    @transaction.amount_paid += @transaction.item_cost
+    @register.accept_money(@transaction)
+    assert_equal(550, @register.total_money)
+  end
 end

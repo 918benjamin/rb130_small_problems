f54476d134dfbafd02ee8d8242fec6889c90976c
Benjamin Perrault <benjamin.perrault@gmail.com>
2021-04-23 07:43:53 -0500

    Add first two tests to cash register test

--- medium2_testing/attempt_two/cash_register_test.rb
+++ medium2_testing/attempt_two/cash_register_test.rb
@@ -6,5 +6,19 @@ require_relative 'cash_register'
 require_relative 'transaction'
 
 class CashRegisterTest < MiniTest::Test
+  def test_accept_money
+    register = CashRegister.new(250)
+    transaction = Transaction.new(25)
+    transaction.amount_paid = 25
+    register.accept_money(transaction)
+    assert_equal 275, register.total_money
+  end
 
+  def test_change
+    register = CashRegister.new(1000)
+    transaction = Transaction.new(25)
+    transaction.amount_paid = 30
+    change = register.change(transaction)
+    assert_equal 5, change
+  end
 end
